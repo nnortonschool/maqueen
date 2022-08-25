113e877b0440b44297e2361a94e1d313b5627bad
nnortonschool <72471414+nnortonschool@users.noreply.github.com>
2022-08-25 13:16:42 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,8 +3,17 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(500)
     music.stopAllSounds()
 
-    
+    move(20)
+    turnLeft()
+    move(630)
+    turnRight()
 });
+
+function move(amnt: any) {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
+    basic.pause(amnt / 0.087)
+    maqueen.motorStop(maqueen.Motors.All)
+}
 
 function turnRight() {
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 75)
