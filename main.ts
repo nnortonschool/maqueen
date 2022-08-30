184fd52bec52dae9e33186f0f0714579de140165
nnortonschool <72471414+nnortonschool@users.noreply.github.com>
2022-08-30 13:05:55 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -6,16 +6,44 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(500)
     music.stopAllSounds()
 
-    move(200)
-    turn(LEFT)
-    move(660)
-    turn(RIGHT)
-    move(300)
-    basic.pause(1000)
-    move(145)
-    turn(LEFT)
-    move(1000)
+    // move(200)
+    // turn(LEFT)
+    // move(660)
+    // turn(RIGHT)
+    // move(300)
+    // basic.pause(1000)
+    // move(145)
+    // turn(LEFT)
+    // move(1000)
+
+    let path: any = [{action: "fwd", distance: 200},
+                     {action: "left"}, 
+                     {action: "fwd", distance: 660},
+                     {action: "right"},
+                     {action: "fwd", distance: 300},
+                     {action: "pause", time: 1000},
+                     {action: "fwd", distance: 145},
+                     {action: "left"},
+                     {action: "fwd", distance: 1000}]
+    parse(path)
 });
+
+function parse(path: any) {
+    path.forEach((item: any) => {
+        switch (item.distance) {
+            case "fwd":
+                move(item.distance)
+            case "left":
+                turn(LEFT)
+            case "right":
+                turn(RIGHT)
+            case "pause":
+                basic.pause(item.time)
+            default:
+                return
+        }
+    })
+}
 
 function move(amnt: any) {
     maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
