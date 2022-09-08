254874465d79fde920badf07b503fcb1b5ec774f
nnortonschool <72471414+nnortonschool@users.noreply.github.com>
2022-09-08 13:32:05 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,59 +6,33 @@ input.onButtonPressed(Button.A, function () {
     basic.pause(500)
     music.stopAllSounds()
 
-    // move(200)
-    // turn(LEFT)
-    // move(660)
-    // turn(RIGHT)
-    // move(300)
-    // basic.pause(1000)
-    // move(145)
-    // turn(LEFT)
-    // move(1000)
-
-    let path: {action: string, amount: number}[] = [{ action: "fwd", amount: 200 },
-    { action: "left", amount: 0 },
-    { action: "fwd", amount: 660 },
-    { action: "right", amount: 0 },
-    { action: "fwd", amount: 300 },
-    { action: "pause", amount: 1000 },
-    { action: "fwd", amount: 145 },
-    { action: "left", amount: 0 },
-    { action: "fwd", amount: 1000 }]
-    parse(path)
+    move(200)
+    turn(LEFT)
+    move(685)
+    turn(RIGHT)
+    move(315)
+    basic.pause(1000)
+    move(160)
+    turn(LEFT)
+    move(1500)
 });
 
-function parse(path: Array<Object>) {
-    path.forEach((item: {action: string, amount: number}) => {
-            switch (item.action) {
-                case "fwd":
-                    move(item.amount)
-                case "left":
-                    turn(LEFT)
-                case "right":
-                    turn(RIGHT)
-                case "pause":
-                    basic.pause(item.amount)
-                default:
-                    return
-        }
-    })
-}
-
 function move(amnt: any) {
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 100)
-    basic.pause(amnt / 0.087)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    basic.pause(amnt / 0.130)
     maqueen.motorStop(maqueen.Motors.All)
 }
 
-function turn(dir: any, ms: any = 900) {
+function turn(dir: any, ms: any = 855) {
     if (dir == 'left') {
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 75 / 2)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 75 / 2)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 75)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 75)
+        basic.pause(ms / 2)
+        maqueen.motorStop(maqueen.Motors.All)
     } else {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 75 / 2)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 75 / 2)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 75)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 75)
+        basic.pause(ms / 2)
+        maqueen.motorStop(maqueen.Motors.All)
     }
-    basic.pause(ms * 2)
-    maqueen.motorStop(maqueen.Motors.All)
 }
